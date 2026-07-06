e3388dec56e6961163479a33e545f9dc19c20214
cavalcante-creator <cavalcante@colafix.com>
2026-07-06 10:08:32 -0300

    Delete js directory

--- js/config-sheets.js
+++
@@ -1,8 +0,0 @@
-// ===== Configuração de acesso ao Google Sheets / Apps Script =====
-// ─── CONFIG SHEETS ────────────────────────────────────────────────
-const SHEETS_API_KEY='AIzaSyCZtWn9oektGVAHxLpZ7V0_qgm_PxOEdsQ';
-const SHEETS_ID='1Zix9W2G6QuwyfkhsFrtNVcXWYGCbEuc9M-TP37JUk6Q';
-const APPS_SCRIPT_URL='https://script.google.com/macros/s/AKfycbznsHd6yWHVKYtqDgIowBdRkj6WfE_2V_3dJUQQBpdumhxiYu-0EaoEotxRhZfCMwZO/exec';
-const RANGE_SALDO='SALDO!A2:D';
-const RANGE_CAPACIDADE='CAPACIDADE !A2:E';
-const RANGE_INVENTARIO_ITENS='INVENTARIO_ITENS!A2:F';
