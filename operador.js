9950d287994dfbffd679e376926179d287845cee
cavalcante-creator <cavalcante@colafix.com>
2026-07-06 10:05:38 -0300

    Delete operador.js

--- operador.js
+++
@@ -1,8 +0,0 @@
-// ===== Inicialização da aplicação =====
-// ─── INIT ─────────────────────────────────────────────────────────
-setStep(0);
-// Inicializar filtro período com hoje
-(function(){
-  const pbHoje=document.getElementById('pbHoje');if(pbHoje)pbHoje.classList.add('active');
-  filtroPeriodo='hoje';
-})();
